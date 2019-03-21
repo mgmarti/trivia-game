f6e71629e7af5288319ad1cb0f5a419e2e4c8d65
moma <mogimartinez@gmail.com>
2019-03-21 10:57:02 -0500

    html updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,63 @@
     const quiz = $('#quiz');
-    const results = $('#results');
     const submit = $('#submit');
+    const totalCorrect = 0;
+    const totalIncorrect = 0;
 
+    const triviaQuestions = [
+        {
+            question: "What movie has a pirate named Jack Sparrow?",
+            answers: {
+                a: "Treasure Island",
+                b: "Pirates of The Caribbean",
+                c: "Hook",
+                d: "Princess Bride"
+            },
+            correctAnswer: "b"
+        },
+        {
+            question: "What's the name of Batman's loyal butler?",
+            answers: {
+                a: "James Stevens",
+                b: "Otis",
+                c: "Alfred Pennyworth",
+                d: "Norman Osborn"
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "What kind of creature is Smaug in The Hobbit Trilogy?",
+            answers: {
+                a: "Orc",
+                b: "Elf",
+                c: "Dwarf",
+                d: "Dragon"
+            },
+            correctAnswer: "d"
+        },
+        {
+            question: "In Transformers, who is the leader of the Autobots?",
+            answers: {
+                a: "Optimus Prime",
+                b: "Megatron",
+                c: "Ironhide",
+                d: "Bumblebee"
+            },
+            correctAnswer: "d"
+        },
+        {
+            question: "What movie features Jack Nicholson as the caretaker of the Overlook Hotel?",
+            answers: {
+                a: "The Shining",
+                b: "Batman",
+                c: "Mars Attacks!",
+                d: "Anger Management"
+            },
+            correctAnswer: "a"
+        }
 
-function buildQUiz() {
+    ];
+
+function buildQuiz() {
 
 }
 
