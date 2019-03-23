3f823d9f89e7bc3646e987e507c0e15fa22469cc
moma <mogimartinez@gmail.com>
2019-03-22 20:01:58 -0500

    js update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,18 +9,43 @@ $(document).ready(function() {
     "What kind of creature is Smaug in The Hobbit Trilogy?", "In Transformers, who is the leader of the Autobots?", 
     "What movie features Jack Nicholson as the caretaker of the Overlook Hotel?"];
 
+    var answers = ["Pirates of The Caribbean", ];
+    var optionOne = ["Treasure Island", "James Stevens", "Orc", "Optimus Prime"];
+    var optionTwo = ["Pirates of The Caribbean", "Otis", "Elf", "Megatron"];
+    var optionThree = ["Hook", "Norman Osborn", "Dragon", "Ironhide"];
+    var optionFour = [];
 
     //display at start
     $('#answer-space').hide();
     $('#question-space').hide();
 
 
-    //functions
+    //question one
     $('#click-start').on('click', () => {
         $('#click-start').hide();
         $('#question-space').show();
         $('#question-space').html(triviaQuestions[questionCount]);
         $('#answer-space').show();
-
+        $('#option-one').html(optionOne[0]);
+        $('#option-two').html(optionTwo[0]);
+        $('#option-three').html(optionThree[0]);
+        $('#option-four').html(optionFour[0]);
     });
+
+    $('#option-two').on('click', () => {
+        $('#answer-space').show();
+        $('#answer-space').html("Wrong! answer is...");
+        correctAnswers++
+    });
+
+    $('#option-one')
+
+    //question two
+
+    //question three
+
+    //question four
+
+    //question five
+
 });
