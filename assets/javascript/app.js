1c989d2387613cca446738a8661f2ead9dde66b0
monica <mogimartinez@gmail.com>
2019-05-08 16:57:05 -0500

    added timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,5 @@
 $("#start").on("click", function () {
-    $("#start").remove();
-    for (var i = 0; i < questions.length; i++) {
-        $("#subwrapper").append("<h2>" + questions[i].question + "</h2>");
-        for (var j = 0; j < questions[i].answers.length; j++) {
-            $("#subwrapper").append("<input type='radio' name='question-" + i + "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j])
-        }
-    }
+    game.start();
 });
 
 var questions = [{
@@ -39,13 +33,24 @@ var questions = [{
 let game = {
     correct: 0,
     incorrect: 0,
-    timer: 120,
+    counter: 120,
     countdown: function () {
         game.counter--;
         $("#counter").html(game.counter);
         if (game.counter === 0) {
             console.log("times up");
-            game.done();
+            
+        }
+    },
+    start: function () {
+        timer = setInterval(game.countdown, 1000);
+        $("#subwrapper").prepend("<h2> Time Remaining: <span id='counter'>120</span> Seconds</h2>");
+        $("#start").remove();
+        for (var i = 0; i < questions.length; i++) {
+            $("#subwrapper").append("<h2>" + questions[i].question + "</h2>");
+            for (var j = 0; j < questions[i].answers.length; j++) {
+                $("#subwrapper").append("<input type='radio' name='question-" + i + "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j])
+            }
         }
     }
 }
